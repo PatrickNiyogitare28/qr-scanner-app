e4776ccf03d58bf6770304eef05751840627654c
Patrick Niyogitare <patrickniyogitare28@gmail.com>
2023-07-18 15:10:04 +0200

    Added test data

---
+++ lib/app/common/modals/Ticket.dart
@@ -0,0 +1,7 @@
+class Ticket {
+  final String title;
+  final String personName;
+  final String time;
+
+  Ticket({required this.title, required this.personName, required this.time});
+}
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:scan_app/app/common/modals/Ticket.dart';
 import 'package:scan_app/app/common/utils/exports.dart';
 import 'package:scan_app/app/modules/home/controllers/home_controller.dart';
 import 'package:scan_app/app/modules/widgets/custom_appbar_widget.dart';
@@ -9,14 +10,48 @@ class HomeView extends GetView<HomeController> {
 
   @override
   Widget build(BuildContext context) {
+    final List<Ticket> dummyTickets = [
+    Ticket(title: 'Sample Ticket 1', personName: 'John Doe', time: '9:00 AM'),
+    Ticket(title: 'Sample Ticket 2', personName: 'Jane Smith', time: '11:30 AM'),
+    Ticket(title: 'Sample Ticket 3', personName: 'Bob Johnson', time: '2:45 PM'),
+    Ticket(title: 'Sample Ticket 3', personName: 'Bob Johnson', time: '2:45 PM'),
+    Ticket(title: 'Sample Ticket 3', personName: 'Bob Johnson', time: '2:45 PM'),
+    Ticket(title: 'Sample Ticket 3', personName: 'Bob Johnson', time: '2:45 PM'),
+    Ticket(title: 'Sample Ticket 3', personName: 'Bob Johnson', time: '2:45 PM'),
+    Ticket(title: 'Sample Ticket 3', personName: 'Bob Johnson', time: '2:45 PM'),
+    Ticket(title: 'Sample Ticket 3', personName: 'Bob Johnson', time: '2:45 PM'),
+    Ticket(title: 'Sample Ticket 3', personName: 'Bob Johnson', time: '2:45 PM'),
+    Ticket(title: 'Sample Ticket 3', personName: 'Bob Johnson', time: '2:45 PM'),
+    // Add more dummy tickets here as needed
+  ];
+
+
     return Scaffold(
       appBar: CustomAppbarWidget(
         addBackButton: false,
-        title: 'Scan app',
+        title: 'Tickets',
+        centerTitle: false,
       ),
-      body:Center(
-        child: Text("The Ticket Scan App."),
-      )
+      body:ListView.builder(
+      itemCount: dummyTickets.length,
+      itemBuilder: (context, index) {
+        final ticket = dummyTickets[index];
+        return ListTile(
+          title: Text(ticket.title),
+          subtitle: Text('${ticket.personName} • ${ticket.time}'),
+          onTap: () {
+            // Add your navigation logic here if needed
+          },
+        );
+      },
+    ),
+       floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your scanning logic here
+          // This will be triggered when the button is pressed
+        },
+        child: Icon(Icons.qr_code),
+      ),
     );
   }
 }
